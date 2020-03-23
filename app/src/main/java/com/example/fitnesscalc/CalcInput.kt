cfa9ddec0ad5d82ddc3fd547328b1939082d96ad
ubunto <JavaCoffeeBean50@gmail.com>
2020-03-23 15:30:30 -0500

    finished radio button for gender

--- app/src/main/java/com/example/fitnesscalc/CalcInput.kt
+++ app/src/main/java/com/example/fitnesscalc/CalcInput.kt
@@ -1,10 +1,21 @@
 package com.example.fitnesscalc
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.properties.Delegates
 
 class CalcInput : AppCompatActivity() {
+
+    lateinit var radiobutton: RadioButton
+    lateinit var radioGroup: RadioGroup
+    var genderMale by Delegates.notNull<Boolean>()
+    lateinit var weightt: EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +25,8 @@ class CalcInput : AppCompatActivity() {
         val calcName2 = intent.getStringExtra("EXTRA_BMI")
 
         val calcTitle: TextView = findViewById(R.id.calcnamE)
+        radioGroup = findViewById(R.id.radioGroup)
+        weightt = findViewById(R.id.weightInput)
 
         try {
 
@@ -28,5 +41,22 @@ class CalcInput : AppCompatActivity() {
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+    }
+
+    fun checkButton(v:View) {
+        val radioId: Int = radioGroup.checkedRadioButtonId
+
+        radiobutton = findViewById(radioId)
+
+        if (radiobutton.text.toString() == "Male") {
+            genderMale = true
+        }else{
+            genderMale = false
+        }
+
+        weightt.setText(genderMale.toString())
+
+
+
     }
 }
