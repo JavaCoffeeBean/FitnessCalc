43cbff1a497934d0901ddc45c6bbb103dcf0f330
ubunto <JavaCoffeeBean50@gmail.com>
2020-03-29 13:39:10 -0500

    fixed bug regarding BMI score showing 0

--- app/src/main/java/com/example/fitnesscalc/BmiResults.kt
+++ app/src/main/java/com/example/fitnesscalc/BmiResults.kt
@@ -1,6 +1,7 @@
 package com.example.fitnesscalc
 
 import android.os.Bundle
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,12 +13,18 @@ class BmiResults : AppCompatActivity() {
         setContentView(R.layout.activity_bmi_results)
 
         val button: TextView = findViewById(R.id.BMI_result)
+        val underweight: RelativeLayout = findViewById(R.id.)
+        val normal: RelativeLayout = findViewById(R.id.)
+        val overweight: RelativeLayout = findViewById(R.id.)
+        val obese: RelativeLayout = findViewById(R.id.)
+        val veryObese: RelativeLayout = findViewById(R.id.)
 
 
 
         val feetHeight: Int = intent.getIntExtra("FEET", 0)
         val inchesHeight: Int =intent.getIntExtra("INCH", 0)
         val weight: Int = intent.getIntExtra("WEIGHT", 0)
+
 
             val finalBMI: Float = forms.calculateBMI(weight,feetHeight, inchesHeight)
 
--- app/src/main/java/com/example/fitnesscalc/Formulas.kt
+++ app/src/main/java/com/example/fitnesscalc/Formulas.kt
@@ -3,15 +3,15 @@ package com.example.fitnesscalc
 class Formulas {
 
     fun calculateBMI(pounds: Int, feet: Int, inches: Int) : Float {
-        val finalHeight: Int = (feet * 12) + inches
-        val finalHeightSquared: Int = finalHeight * finalHeight
+        val finalHeight: Float = (feet.toFloat() * 12) + inches.toFloat()
+        val finalHeightSquared: Float = finalHeight * finalHeight
 
 
-        val finalBMI = (pounds/finalHeightSquared) * 703
+        val finalBMI = (pounds.toFloat()/finalHeightSquared) * 703
 
 
 
-        return finalBMI.toFloat()
+        return finalBMI
 
     }
 }
